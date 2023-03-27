3cb81818da708999240425191cc48b635b6714cd
Fazalul Abid <fazalulabid007@gmail.com>
2023-03-27 16:50:23 +0530

    Project created.

---
+++ app/src/main/java/com/fifty/learnpushnotificationusingfirebase/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.fifty.learnpushnotificationusingfirebase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fifty.learnpushnotificationusingfirebase.ui.theme.LearnPushNotificationUsingFirebaseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LearnPushNotificationUsingFirebaseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LearnPushNotificationUsingFirebaseTheme {
+        Greeting("Android")
+    }
+}
