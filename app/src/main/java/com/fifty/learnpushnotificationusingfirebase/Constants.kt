5726cb60ff2bd1d581523550b8ccbcf027bbd6f4
Fazalul Abid <fazalulabid007@gmail.com>
2023-03-29 20:43:22 +0530

    Push notification with firebase completed.

---
+++ app/src/main/java/com/fifty/learnpushnotificationusingfirebase/Constants.kt
@@ -0,0 +1,10 @@
+package com.fifty.learnpushnotificationusingfirebase
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY =
+            "AAAA8E3Qjbw:APA91bHpPlS4Xo6PUFhdb2EFNS-RPCQr1faIGsxL2CS0hTqT5F38mcCxqeEM06t6ZY5dTxUUXRvBXucM8iQ91dd0grpox-PLWHNfWc04iwa_vK5jyGJ4YhX5usDpS_3MBemD3GydVASP"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
---
+++ app/src/main/java/com/fifty/learnpushnotificationusingfirebase/NotificationData.kt
@@ -0,0 +1,6 @@
+package com.fifty.learnpushnotificationusingfirebase
+
+data class NotificationData(
+    val title: String,
+    val message: String
+)
